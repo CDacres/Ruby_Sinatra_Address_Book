866bee1d246e71c6c34fc0fe97115161d5226dcb
Christian Dacres <christian_dacres@hotmail.co.uk>
2017-09-28 16:56:31 +0100

    added views directory and files

--- controllers/address_controller.rb
+++ controllers/address_controller.rb
@@ -2,4 +2,8 @@ class AddressController < Sinatra::Base
 	configure :development do 
         register Sinatra::Reloader
 	end
+
+	set :root, File.join(File.dirname(__FILE__), "..")
+	set :views, Proc.new { File.join(root, 'views') }
+
 end

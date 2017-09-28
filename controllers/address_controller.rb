492aa0bde2d637cde615514034c40bcee5d93b09
Christian Dacres <christian_dacres@hotmail.co.uk>
2017-09-28 21:11:32 +0100

    added addresses

--- controllers/address_controller.rb
+++ controllers/address_controller.rb
@@ -6,4 +6,25 @@ class AddressController < Sinatra::Base
 	set :root, File.join(File.dirname(__FILE__), "..")
 	set :views, Proc.new { File.join(root, 'views') }
 
+	$addresses = [{
+	  name: "Harry Potter",
+	  address: "The Cupboard Under the Stairs, 4 Privet Drive, Little Whinging, Surrey, England, Great Britain"
+	},
+	{
+	  name: "Sherlock Holmes",
+	  address: "221B Baker Street, London, England, Great Britain"
+	},
+	{
+	  name: "Paddington Bear",
+	  address: "32 Windsor Gardens, London, England, Great Britain"
+	},
+	{
+	  name: "Luna Lovegood"
+	  address: "Lovegood House,Ottery St Catchpole, Devon, England, Great Britain"
+	},
+	{
+	  name: "Spongebob Squarepants"
+	  address: "124 Conch Street, Bikini Bottom, Pacific Ocean"
+	}]
+
 end
